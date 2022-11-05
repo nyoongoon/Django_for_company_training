f38502d245338ad2c4f62d7b3dc33d0f3b7e2edf
Yoon <nyoongoon@naver.com>
2022-11-05 16:44:46 +0900

    프론트엔드 적용

--- do_it_django_a_to_z/blog/urls.py
+++ do_it_django_a_to_z/blog/urls.py
@@ -4,7 +4,8 @@ from . import views # urls.py에 들어갈 함수 및 클래스는 views.py에 �
 # CBV(Class Based View) : 장고에서 제공하는 클래스를 이용한 정의
 
 urlpatterns = [
-    path('<int:pk>/', views.single_post_page),
-    path('', views.index),
-    path('', include('single_pages.urls')),
+    path('<int:pk>/', views.PostDetail.as_view()),
+    # path('', views.index),
+    path('', views.PostList.as_view()),
+    # path('', include('single_pages.urls')),
 ]
--- do_it_django_a_to_z/blog/views.py
+++ do_it_django_a_to_z/blog/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
+
 from .models import Post
+from django.views.generic import ListView, DetailView
 
 
-# Create your views here.
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
-    return render(request, 'blog/index.html', {'posts': posts, })
+# class 기반
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+
+class PostDetail(DetailView):
+    model = Post
 
 
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)
-    return render(request, 'blog/single_post_page.html', {'post': post, })
+
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk')
+#     return render(request, 'blog/post_list.html', {'posts': posts, })
+#
+#
+# def single_post_page(request, pk):
+#     post = Post.objects.get(pk=pk)
+#     return render(request, 'blog/post_detail.html', {'post': post, })
